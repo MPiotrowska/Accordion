41880f191fe13847b08e2f48c2eff1cd0171d095
Monika Piotrowska <monikapiotrowska27@gmail.com>
2023-09-14 14:00:39 +0200

    add keybord nav through acordion

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -7,16 +7,13 @@
             <div class="accordion-content-section">
                 <div class="accordion-name">{{ tag }}</div>
                 <div class="accordion-title">{{ title }}</div>
-                <div v-for="(item, index) in items" :key="index" class="accordion-item"
-                    :class="{ 'active': index === activeTab }" @click="toggleAccordion(index)">
+                <div v-for="(item, index) in items" :key="index" class="accordion-item" tabindex="0" @keydown.up="prevTab"
+                    @keydown.down="nextTab" :class="{ 'active': index === activeTab }" @click="toggleAccordion(index)">
                     <div class="accordion-item-svg-section">
                         <div class="accordion-item-title" :class="{ 'active': index === activeTab }">{{ item.title }}</div>
                         <img class='accordion-item-arrow' :class="{ 'active': index === activeTab }"
                             src="../assets/svg/arrow.svg" alt="arrow" />
                     </div>
-
-
-
                     <div class="accordion-item-description" v-if="index === activeTab">{{ item.text }}</div>
                     <div class="accordion-item-image" v-if="index === activeTab"><img :src="items[activeTab].image"
                             alt="Accordion Image" />
@@ -43,6 +40,12 @@ export default {
     methods: {
         toggleAccordion(index) {
             this.activeTab = index;
+        },
+        prevTab() {
+            this.activeTab = (this.activeTab - 1 + this.items.length) % this.items.length;
+        },
+        nextTab() {
+            this.activeTab = (this.activeTab + 1) % this.items.length;
         },
     },
     computed: {
@@ -84,7 +87,6 @@ export default {
         align-items: center;
         margin-top: $space-xl;
 
-
         @media (max-width: $large) {
             justify-content: space-between;
 
@@ -102,8 +104,6 @@ export default {
                 padding: 18px;
                 border-radius: 3px;
                 margin-top: $space-sm;
-
-
                 background: $general-background;
 
 
@@ -127,7 +127,6 @@ export default {
                     color: $general-subheading-color;
                     line-height: $text-line-height-subheading;
 
-
                     &.active {
                         color: $active-text-color;
                     }
@@ -144,10 +143,6 @@ export default {
                     font-weight: $text-weight-general;
                     padding-right: $space-xl;
                     padding-top: $space-sm;
-
-
-
-
 
                     @media (max-width: $large) {
                         font-size: $text-size-general-mobile;
@@ -167,13 +162,9 @@ export default {
 
                         &.active {
                             transform: rotate(360deg);
-
                         }
 
-
                     }
-
-
 
                 }
             }
